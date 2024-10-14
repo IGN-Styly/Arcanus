a76e92fe91f9f68ea0482b07df7c2c999f8cbcbb
IGN-Styly <claudioresendeptbr@gmail.com>
2024-10-14 20:20:44 +0100

    add support for recipe datagen

---
+++ src/main/java/org/styly/arcanus/recipe/RitualRecipeBuilder.java
@@ -0,0 +1,33 @@
+package org.styly.arcanus.recipe;
+
+import net.minecraft.advancements.Advancement;
+import net.minecraft.advancements.AdvancementRequirements;
+import net.minecraft.advancements.AdvancementRewards;
+import net.minecraft.advancements.critereon.RecipeUnlockedTrigger;
+import net.minecraft.data.recipes.RecipeOutput;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+
+import java.util.List;
+
+public class RitualRecipeBuilder extends SimpleRecipeBuilder{
+    private final List<ItemStack> inputItems;
+    public RitualRecipeBuilder(ItemStack result,List<ItemStack> inputItems){
+        super(result);
+        this.inputItems=inputItems;
+    }
+    // Saves a recipe using the given RecipeOutput and id. This method is defined in the RecipeBuilder interface.
+    @Override
+    public void save(RecipeOutput output, ResourceLocation id) {
+        // Build the advancement.
+        Advancement.Builder advancement = output.advancement()
+                .addCriterion("has_the_recipe", RecipeUnlockedTrigger.unlocked(id))
+                .rewards(AdvancementRewards.Builder.recipe(id))
+                .requirements(AdvancementRequirements.Strategy.OR);
+        this.criteria.forEach(advancement::addCriterion);
+        // Our factory parameters are the result, the block state, and the ingredient.
+        RitualRecipe recipe = new RitualRecipe(this.inputItems, this.result);
+        // Pass the id, the recipe, and the recipe advancement into the RecipeOutput.
+        output.accept(id, recipe, advancement.build(id.withPrefix("recipes/")));
+    }
+}
---
+++ src/main/java/org/styly/arcanus/recipe/SimpleRecipeBuilder.java
@@ -0,0 +1,47 @@
+package org.styly.arcanus.recipe;
+
+import net.minecraft.advancements.Criterion;
+import net.minecraft.data.recipes.RecipeBuilder;
+import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.Nullable;
+import net.minecraft.world.item.Item;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+// This class is abstract because there is a lot of per-recipe-serializer logic.
+// It serves the purpose of showing the common part of all (vanilla) recipe builders.
+public abstract class SimpleRecipeBuilder implements RecipeBuilder {
+    // Make the fields protected so our subclasses can use them.
+    protected final ItemStack result;
+    protected final Map<String, Criterion<?>> criteria = new LinkedHashMap<>();
+    @Nullable
+    protected String group;
+
+    // It is common for constructors to accept the result item stack.
+    // Alternatively, static builder methods are also possible.
+    public SimpleRecipeBuilder(ItemStack result) {
+        this.result = result;
+    }
+
+    // This method adds a criterion for the recipe advancement.
+    @Override
+    public SimpleRecipeBuilder unlockedBy(String name, Criterion<?> criterion) {
+        this.criteria.put(name, criterion);
+        return this;
+    }
+
+    // This method adds a recipe book group. If you do not want to use recipe book groups,
+    // remove the this.group field and make this method no-op (i.e. return this).
+    @Override
+    public SimpleRecipeBuilder group(@Nullable String group) {
+        this.group = group;
+        return this;
+    }
+
+    // Vanilla wants an Item here, not an ItemStack. You still can and should use the ItemStack
+    // for serializing the recipes.
+    @Override
+    public Item getResult() {
+        return this.result.getItem();
+    }
+}
