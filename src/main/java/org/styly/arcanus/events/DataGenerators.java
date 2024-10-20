f313089c4b506dcaa13c55f4a4f56f1166f6f85f
IGN-Styly <claudioresendeptbr@gmail.com>
2024-10-20 19:36:50 +0100

    it all works with datagen

--- src/main/java/org/styly/arcanus/events/DataGenerators.java
+++ src/main/java/org/styly/arcanus/events/DataGenerators.java
@@ -1,22 +1,25 @@
 package org.styly.arcanus.events;
 
+import io.redspace.ironsspellbooks.IronsSpellbooks;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
+import org.styly.arcanus.Arcanus;
 import org.styly.arcanus.recipe.RitualRecipeProvider;
 
 import java.util.concurrent.CompletableFuture;
 
-
+@EventBusSubscriber(modid = Arcanus.MODID, bus = EventBusSubscriber.Bus.MOD)
 public class RegisterRecipes {
     @SubscribeEvent
     public static void gatherData(GatherDataEvent event) {
         DataGenerator generator = event.getGenerator();
         PackOutput output = generator.getPackOutput();
         CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
-
+        Arcanus.LOGGER.warn("Styly: I exist");
         // other providers here
         generator.addProvider(
                 event.includeServer(),
