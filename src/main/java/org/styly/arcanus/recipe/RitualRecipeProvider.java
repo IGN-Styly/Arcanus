19f7a6a9b3c850f449e938b3003c817f68aa50bb
IGN-Styly <claudioresendeptbr@gmail.com>
2024-11-04 22:05:42 +0000

    work

--- src/main/java/org/styly/arcanus/recipe/RitualRecipeProvider.java
+++ src/main/java/org/styly/arcanus/recipe/RitualRecipeProvider.java
@@ -3,9 +3,14 @@ package org.styly.arcanus.recipe;
 import io.redspace.ironsspellbooks.registries.ItemRegistry;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
+import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.data.recipes.SmithingTransformRecipeBuilder;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
 import org.styly.arcanus.Arcanus;
 import org.styly.arcanus.registry.ModItems;
 
@@ -19,6 +24,7 @@ public class RitualRecipeProvider extends RecipeProvider {
 
     @Override
     protected void buildRecipes(RecipeOutput output) {
+
         //new RitualRecipeBuilder(Items.NETHERITE_INGOT.getDefaultInstance(), Arrays.asList(ModItems.AIR.toStack(), Items.DIAMOND.getDefaultInstance(), ModItems.AIR.toStack(), ModItems.AIR.toStack(), ModItems.AIR.toStack(), ModItems.AIR.toStack(), ModItems.AIR.toStack(), ModItems.AIR.toStack(), ModItems.AIR.toStack(), ModItems.AIR.toStack(), ModItems.AIR.toStack(), ModItems.AIR.toStack(), ModItems.AIR.toStack(), ModItems.AIR.toStack(), ModItems.AIR.toStack(), ModItems.AIR.toStack(),ModItems.AIR.toStack())).save(output,new ResourceLocation(Arcanus.MODID,"test_recipe2"));
         new RitualRecipeBuilder(ModItems.MANA_VIAL.toStack(), Arrays.asList(
                 ModItems.AIR.toStack(), // must begin with air
